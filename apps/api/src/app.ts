f0db3b5d7b38a9d4630786c66f717f17f435322d
Akhilesh Tiwari <10akhil.t@gmail.com>
2026-06-30 13:47:46 +0530

    feat: CORS setup

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -5,7 +5,12 @@ import { errorHandler } from "./middleware/error-handler";
 
 const app: express.Application = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL ?? "http://localhost:3001",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use(router);
